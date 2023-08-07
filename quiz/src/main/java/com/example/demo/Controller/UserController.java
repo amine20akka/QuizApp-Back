f694b0353232037c57a27b4c2abcc4be7bb3c3b7
amine20akka <amine.akkari@etudiant-enit.utm.tn>
2023-08-07 20:15:14 +0100

    rearranging the request body

--- quiz/src/main/java/com/example/demo/Controller/UserController.java
+++ quiz/src/main/java/com/example/demo/Controller/UserController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.DTO.AuthenticationRequestDTO;
 import com.example.demo.DTO.UserDTO;
-import com.example.demo.Entity.AuthenticationRequestDTO;
 import com.example.demo.Service.UserService;
 
 import lombok.RequiredArgsConstructor;
--- quiz/src/main/java/com/example/demo/DTO/AuthenticationRequestDTO.java
+++ quiz/src/main/java/com/example/demo/DTO/AuthenticationRequestDTO.java
@@ -1,4 +1,4 @@
-package com.example.demo.Entity;
+package com.example.demo.DTO;
 
 import lombok.Data;
 
